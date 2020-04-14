f6b863e585e539da79d6258497f561a4b39904c3
Hasanov-B <boxa1998hasanov18@gmail.com>
2020-04-13 21:15:24 -0400

    verifying new classes

--- src/test/java/com/TryCloud/runners/CucumberRunners.java
+++ src/test/java/com/TryCloud/runners/CucumberRunners.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features"
+        features = "src/test/resources/features",
+        glue = "com/TryCloud/step_definitions"
 )
 public class CucumberRunners {
 
--- src/test/java/com/TryCloud/step_definitions/LoginStepDefinition.java
+++ src/test/java/com/TryCloud/step_definitions/LoginStepDefinition.java
@@ -6,20 +6,18 @@ public class LoginStepDefinition {
 
     @Given("user is on the landing page")
     public void user_is_on_the_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("I am on the login page");
     }
 
     @Then("user enter username and password")
     public void user_enter_username_and_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("I entered user Credentials");
     }
 
     @Then("user verifies that {string}page subtitle is displayed")
     public void user_verifies_that_page_subtitle_is_displayed(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("Verifying page subtitle"+string);
+
     }
 
 
